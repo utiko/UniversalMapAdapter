a2a8ea8b3a708f3eb3cffe5072613dcc1b2bbcb0
Kostia Kolesnyk <tiko@utiko.net>
2017-07-29 17:39:45 +0300

    Initial Commit

---
+++ UniversalMapAdapter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UniversalMapAdapter
+//
+//  Created by Kostia Kolesnyk on 7/29/17.
+//  Copyright © 2017 Kostia Kolesnyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
